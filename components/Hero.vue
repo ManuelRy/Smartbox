36a264b6222ac36999db229bf2b3feae0daf7fa0
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-04 21:14:45 +0700

    hek

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="bg-white dark:bg-gray-900 bg-[url('/img/SmartBox.png')] h-screen bg-cover bg-center bg-no-repeat">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7 mt-52">
+            <div class="mr-auto place-self-center lg:col-span-7 mt-40">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
                 <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
@@ -12,8 +12,13 @@
                     Speak to Sales
                 </a> 
             </div>
-            <div class="hidden lg:col-span-5 lg:flex mt-44">
-                <img src="/img/Application.png" alt="mockup">
+            <div class="hidden lg:col-span-5 grid-cols-2 lg:flex mt-9 h-96">
+                <div>
+                    <img src="/img/p1.png" alt="mockup">
+                </div>
+                <div>
+                    <img src="/img/p2.png" alt="mockup">
+                </div>
             </div>                
         </div>
     </section>
