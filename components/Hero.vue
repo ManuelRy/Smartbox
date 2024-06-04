95c4e596dd9c251630a620fd4a12e82c5a5ac73d
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-04 22:48:13 +0700

    joe

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,10 +2,10 @@
     <section class="bg-white dark:bg-gray-900 bg-[url('/img/SmartBox.png')] h-screen bg-cover bg-center bg-no-repeat"
         id="bg">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
+            <div class="mr-auto place-self-center lg:col-span-7 mt-40">
                 <h1
-                    class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-                    Payments tool for software companies</h1>
+                    class="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">
+                    Your Stress-Free Storage Solution</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From
                     checkout to global sales tax compliance, companies around the world use Flowbite to simplify their
                     payment stack.</p>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,55 +1,94 @@
 <template>
   <div class="justify-center w-full">
     <nav
-      class="bg-white dark:bg-gray-900 fixed z-1000 top-0 left-5 right-5 mx-auto border-b border-gray-200 dark:border-gray-600 items-center ">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      class="bg-white dark:bg-gray-900 fixed z-1000 top-0 left-0 right-0 md:w-full w-full lg:w-9/12 mx-auto border-b border-gray-200 dark:border-gray-600 items-center"
+    >
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      >
         <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/img/logo.png" class="animate__animated animate__bounce h-16" alt="Smartbox Logo" />
+          <img
+            src="/img/logo.png"
+            class="animate__animated animate__bounce h-16"
+            alt="Smartbox Logo"
+          />
         </a>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button @click="toggleMenu" data-collapse-toggle="navbar-sticky" type="button"
+          <button
+            @click="toggleMenu"
+            data-collapse-toggle="navbar-sticky"
+            type="button"
             class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky" aria-expanded="false">
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
             <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15" />
+            <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
             </svg>
           </button>
         </div>
 
-        <div :class="{
-          'hidden': !isMenuOpen,
-          'block': isMenuOpen,
-          'transition-all': true,
-          'duration-300': true,
-          'ease-in-out': true
-        }" class="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <div
+          :class="{
+            'hidden': !isMenuOpen,
+            'block': isMenuOpen,
+            'transition-all': true,
+            'duration-300': true,
+            'ease-in-out': true
+          }"
+          class="items-center justify-between w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
           <ul
-            class="flex flex-col justify-center p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            class="flex flex-col justify-center p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          >
             <li>
-              <a href="#"
+              <a
+                href="#"
                 class="block py-2 px-3 text-lg text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 scroll-smooth focus:scroll-auto"
-                aria-current="page">Home</a>
+                aria-current="page"
+                >HOME</a
+              >
             </li>
             <li>
-              <a href="#About"
-                class="block py-2 px-3 text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 scroll-smooth focus:scroll-auto">About</a>
+              <a
+                href="#About"
+                class="block py-2 px-3 text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 scroll-smooth focus:scroll-auto"
+                >ABOUT</a
+              >
             </li>
             <li>
-              <a href="#Service"
-                class="block py-2 px-3 text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+              <a
+                href="#Service"
+                class="block py-2 px-3 text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >SERVICES</a
+              >
             </li>
             <li>
-              <a href="#Contact"
-                class="block py-2 px-3 text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+              <a
+                href="#Contact"
+                class="block py-2 px-3 text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >CONTACT</a
+              >
             </li>
           </ul>
         </div>
       </div>
     </nav>
   </div>
-
 </template>
 
 <script>
