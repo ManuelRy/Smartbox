87e507eecaf346be31d49601a87da959577044e5
Kosal Vathanak <vathanak110@gmail.com>
2024-06-05 18:12:55 +0700

    added terms and conditions

---
+++ components/T&C.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="bg-orange-200 dark:bg-gray-900">
+    <div class="mx-48">
+      <h1
+        class="mb-4 text-5xl tracking-tight font-sans font-extrabold text-blue-900 dark:text-white text-center"
+      >
+        Terms and Conditions
+      </h1>
+      <p class="text-center font-semibold mb-12 text-2xl font-mono">
+        By agreeing to these Terms and Conditions, the Customer acknowledges
+        that they shall not transport or store any items in SmartBox lockers,
+        including but not limited to certain types of goods.
+      </p>
+    </div>
+    <div class="my-12 flex justify-center items-center ">
+      <img src="/img/T&C.png" alt="fobidden-items" class="rounded-2xl" />
+    </div>
+  </section>
+</template>
