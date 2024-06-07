5062308f46c5cbfafee65e7060dc13042d43567b
Kosal Vathanak <vathanak110@gmail.com>
2024-06-07 14:40:53 +0700

    work on storage

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,7 +7,7 @@
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
       <div class="md:flex md:justify-between">
         <div class="mb-6 md:mb-0">
-          <a href="" class="flex items-center">
+          <a href="/" class="flex items-center">
             <img src="/img/logo.png" class="h-16" alt="SmartBox Logo" />
           </a>
           <p class="ml-12 mb-6">
--- components/Storage.vue
+++ components/Storage.vue
@@ -33,7 +33,6 @@
       <section class="bg-orange-200 dark:bg-gray-900" data-aos="fade-up">
         <div class="px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
           <div class="grid gap-14 md:grid-cols-5 md:gap-2">
-            
             <div
               class="rounded-xl bg-orange-300 p-6 text-center shadow-xl md:w-full"
               data-aos="zoom-in-up"
@@ -53,7 +52,7 @@
               <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
                 Step 1
               </h1>
-              <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
                 Scan QR Code on our locker
               </h1>
             </div>
@@ -110,6 +109,9 @@
               <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
                 Step 2
               </h1>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
+                Choose Package Size
+              </h1>
             </div>
 
             <div
@@ -130,6 +132,9 @@
               <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
                 Step 3
               </h1>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
+                Fill Information
+              </h1>
             </div>
             <div
               class="rounded-xl bg-orange-300 p-6 text-center shadow-xl md:w-full"
@@ -148,6 +153,9 @@
               </div>
               <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
                 Step 4
+              </h1>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
+                Create Your Own PIN
               </h1>
             </div>
             <div
@@ -168,6 +176,88 @@
               </div>
               <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
                 Step 5
+              </h1>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
+                Place your belongings in locker
+              </h1>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="to-takeout flex items-center justify-center">
+      <section class="bg-orange-200 dark:bg-gray-900 pt-0" data-aos="fade-up">
+        <div class="py-2 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div class="grid gap-14 md:grid-cols-5 md:gap-2">
+            <div
+              class="rounded-xl bg-orange-300 p-6 text-center shadow-xl md:w-full col-start-2"
+              data-aos="zoom-in-up"
+              data-aos-delay="200"
+            >
+              <div
+                class="mx-auto flex h-24 w-24 -translate-y-12 transform items-center justify-center rounded-full bg-orange-500 shadow-lg shadow-teal-500/40"
+              >
+                <img
+                  src="/img/storage/qr.png"
+                  alt=""
+                  width="75"
+                  height="75"
+                  srcset=""
+                />
+              </div>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
+                Step 1
+              </h1>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
+                Scan QR Code on our locker
+              </h1>
+            </div>
+
+            <div
+              class="rounded-xl bg-orange-300 p-6 text-center shadow-xl md:w-full"
+              data-aos="zoom-in-up"
+              data-aos-delay="200"
+            >
+              <div
+                class="mx-auto flex h-24 w-24 -translate-y-12 transform items-center justify-center rounded-full bg-orange-500 shadow-lg shadow-teal-500/40"
+              >
+                <img
+                  src="/img/storage/qr.png"
+                  alt=""
+                  width="75"
+                  height="75"
+                  srcset=""
+                />
+              </div>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
+                Step 2
+              </h1>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
+                Collect your items
+              </h1>
+            </div>
+
+            <div
+              class="rounded-xl bg-orange-300 p-6 text-center shadow-xl md:w-full"
+              data-aos="zoom-in-up"
+              data-aos-delay="200"
+            >
+              <div
+                class="mx-auto flex h-24 w-24 -translate-y-12 transform items-center justify-center rounded-full bg-orange-500 shadow-lg shadow-teal-500/40"
+              >
+                <img
+                  src="/img/storage/pin.png"
+                  alt=""
+                  width="75"
+                  height="75"
+                  srcset=""
+                />
+              </div>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-10">
+                Step 3
+              </h1>
+              <h1 class="text-darken mb-3 text-xl font-medium lg:px-6">
+                Close and Lock the door
               </h1>
             </div>
           </div>
