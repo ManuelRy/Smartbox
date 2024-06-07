1c74f7be6f75cddde1c6de2ab86513d448287686
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-07 14:42:06 +0700

    ok

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,18 +2,26 @@
     <section class="bg-white dark:bg-gray-900 bg-[url('/img/hero.jpg')] h-screen bg-cover bg-center bg-no-repeat"
         id="bg">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7 mt-28 bg-yellow-300 rounded-lg">
+            <div class="mr-auto place-self-center lg:col-span-6 mt-28 bg-yellow-300 rounded-lg p-6">
                 <h1
-                    class="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white text-center">
+                    class="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-5xl dark:text-white text-center">
                     Your Stress-Free Storage Solution</h1>
             </div>
-            <div class="hidden lg:flex lg:col-span-5 mt-28 h-5/6 w-72 relative">
+            <!-- <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:hidden mt-6">
+                <div class="w-full">
+                    <img src="/img/p1.png" alt="mockup" class=" w-28  sm:h-56 object-cover">
+                </div>
+                <div class="w-full">
+                    <img src="/img/p2.png" alt="mockup" class=" w-28  sm:h-56 object-cover">
+                </div>
+            </div> -->
+            <div class="hidden lg:flex mt-28 h-5/6 lg:w-72 relative ">
                 <div class="relative h-full">
                     <img src="/img/p1.png" alt="mockup" class="h-full">
                 </div>
-                <div class=" h-full absolute right-[-80%] top-[5%]">
+                <div class="h-full absolute right-[-72%] top-[5%]">
                     <img src="/img/p2.png" alt="mockup" class="h-full">
-                </div>
+                </div> 
             </div>
         </div>
     </section>
