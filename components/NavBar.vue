3f74ce142220595205b5cd83cf9eb4f28003b570
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-08 23:50:47 +0700

    s

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -33,8 +33,10 @@
                 aria-current="page">HOME</a>
             </li>
             <li>
-              <a href="#About"
+              <nuxt-link to="#About">
+                 <a href="#About"
                 class="block py-2 px-3 text-lg text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 scroll-smooth focus:scroll-auto">ABOUT</a>
+              </nuxt-link>
             </li>
             <li>
               <a id="dropdownDefaultButton" data-dropdown-toggle="dropdown" href="#Service"
