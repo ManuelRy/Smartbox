a713e747a96f4e93a8eefe5e71604d0be974030c
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-12 01:10:40 +0700

    hero and animation

--- components/About-us.vue
+++ components/About-us.vue
@@ -1,100 +1,102 @@
 <template>
-  <section class="bg-white dark:bg-gray-900">
-    <div class="px-4 mx-auto max-w-screen-xl py-16 lg:px-6">
-      <div class="about-us" data-aos="fade-up">
-        <div class="mb-16">
-          <h1
-            class="mb-4 text-4xl sm:text-5xl tracking-tight font-montserrat font-bold text-blue-900 dark:text-white text-center"
-          >
-            ABOUT US
-          </h1>
-        </div>
-        <div class="mb-16">
-          <h3
-            class="text-center mb-12 text-lg sm:text-2xl font-montserrat font-semibold text-balance"
-          >
-            SmartBox is a digital locker base in Cambodia, located in the most
-            convenient locations, easing the common hassle of having too much to
-            carry.
-          </h3>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="bg-white dark:bg-gray-900">
-    <div class="px-4 mx-auto max-w-screen-xl py-16 lg:px-6">
-      <div class="why-smartbox">
-        <div data-aos="fade-down">
+  <div>
+    <section class="bg-white dark:bg-gray-900">
+      <div class="px-4 mx-auto max-w-screen-xl py-16 lg:px-6">
+        <div class="about-us overflow-hidden" data-aos="fade-up">
           <div class="mb-16">
             <h1
               class="mb-4 text-4xl sm:text-5xl tracking-tight font-montserrat font-bold text-blue-900 dark:text-white text-center"
             >
-              Why SmartBox?
+              ABOUT US
             </h1>
           </div>
-          <div>
+          <div class="mb-16">
             <h3
               class="text-center mb-12 text-lg sm:text-2xl font-montserrat font-semibold text-balance"
             >
-              Our digital lockers are accessible 24/7 at convenient locations.
-              Pick up, drop off and store your items when and where it suits
-              you.
+              SmartBox is a digital locker base in Cambodia, located in the most
+              convenient locations, easing the common hassle of having too much to
+              carry.
             </h3>
           </div>
         </div>
-        <div
-          class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0"
-        >
-          <div
-            class="max-w-xs mx-auto rounded-md"
-            data-aos="fade-right"
-            data-aos-delay="200"
-          >
-            <img
-              src="/img/about/convinient.png"
-              alt="Convenient"
-              class="w-32 h-32 sm:w-40 sm:h-40 lg:w-60 lg:h-60 mx-auto"
-            />
-            <h2
-              class="text-blue-900 dark:text-white text-center text-xl sm:text-2xl lg:text-4xl font-bold font-montserrat mt-4"
-            >
-              Convenient
-            </h2>
+      </div>
+    </section>
+    <section class="bg-white dark:bg-gray-900">
+      <div class="px-4 mx-auto max-w-screen-xl py-16 lg:px-6 overflow-hidden">
+        <div class="why-smartbox">
+          <div data-aos="fade-down" class="overflow-hidden">
+            <div class="mb-16">
+              <h1
+                class="mb-4 text-4xl sm:text-5xl tracking-tight font-montserrat font-bold text-blue-900 dark:text-white text-center"
+              >
+                Why SmartBox?
+              </h1>
+            </div>
+            <div>
+              <h3
+                class="text-center mb-12 text-lg sm:text-2xl font-montserrat font-semibold text-balance"
+              >
+                Our digital lockers are accessible 24/7 at convenient locations.
+                Pick up, drop off and store your items when and where it suits
+                you.
+              </h3>
+            </div>
           </div>
           <div
-            class="max-w-xs mx-auto rounded-md"
-            data-aos="fade-up"
-            data-aos-delay="400"
+            class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0"
           >
-            <img
-              src="/img/about/secure.png"
-              alt="Secure"
-              class="w-32 h-32 sm:w-40 sm:h-40 lg:w-60 lg:h-60 mx-auto"
-            />
-            <h2
-              class="text-blue-900 dark:text-white text-center text-xl sm:text-2xl lg:text-4xl font-bold font-montserrat mt-4"
+            <div
+              class="max-w-xs mx-auto rounded-md overflow-hidden"
+              data-aos="fade-right"
+              data-aos-delay="200"
             >
-              Secure
-            </h2>
-          </div>
-          <div
-            class="max-w-xs mx-auto rounded-md"
-            data-aos="fade-left"
-            data-aos-delay="600"
-          >
-            <img
-              src="/img/about/24.png"
-              alt="24/7"
-              class="w-32 h-32 sm:w-40 sm:h-40 lg:w-60 lg:h-60 mx-auto"
-            />
-            <h2
-              class="text-blue-900 dark:text-white text-center text-xl sm:text-2xl lg:text-4xl font-bold font-montserrat mt-4"
+              <img
+                src="/img/about/convinient.png"
+                alt="Convenient"
+                class="w-32 h-32 sm:w-40 sm:h-40 lg:w-60 lg:h-60 mx-auto"
+              />
+              <h2
+                class="text-blue-900 dark:text-white text-center text-xl sm:text-2xl lg:text-4xl font-bold font-montserrat mt-4"
+              >
+                Convenient
+              </h2>
+            </div>
+            <div
+              class="max-w-xs mx-auto rounded-md overflow-hidden"
+              data-aos="fade-up"
+              data-aos-delay="400"
             >
-              24/7
-            </h2>
+              <img
+                src="/img/about/secure.png"
+                alt="Secure"
+                class="w-32 h-32 sm:w-40 sm:h-40 lg:w-60 lg:h-60 mx-auto"
+              />
+              <h2
+                class="text-blue-900 dark:text-white text-center text-xl sm:text-2xl lg:text-4xl font-bold font-montserrat mt-4"
+              >
+                Secure
+              </h2>
+            </div>
+            <div
+              class="max-w-xs mx-auto rounded-md overflow-hidden"
+              data-aos="fade-left"
+              data-aos-delay="600"
+            >
+              <img
+                src="/img/about/24.png"
+                alt="24/7"
+                class="w-32 h-32 sm:w-40 sm:h-40 lg:w-60 lg:h-60 mx-auto"
+              />
+              <h2
+                class="text-blue-900 dark:text-white text-center text-xl sm:text-2xl lg:text-4xl font-bold font-montserrat mt-4"
+              >
+                24/7
+              </h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
