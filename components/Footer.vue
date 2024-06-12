3bc30e6ae58f180eccec94974cc60c2c0da47474
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-12 21:16:48 +0700

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,12 @@
 <template>
-  <footer
-    class="bg-gray-100 dark:bg-gray-900 overflow-hidden font-montserrat"
-    data-aos="fade-up"
-  >
+  <footer class="bg-gray-100 dark:bg-gray-900 overflow-hidden font-montserrat" data-aos="fade-up">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
       <div class="md:flex md:justify-between">
         <div class="mb-6 md:mb-0">
           <a href="/" class="flex items-center">
             <img src="/img/logo.png" class="h-16" alt="SmartBox Logo" />
           </a>
+          <h2 class="ml-12 mt-2 mb-6 text-lg font-extrabold">Contact Us</h2>
           <p class="ml-12 mb-6">
             <Icon name="fa6-solid:envelope" />
             hello.wearesmartbox@gmail.com
@@ -24,9 +22,7 @@
         </div>
         <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
           <div>
-            <h2
-              class="mb-6 text-md font-semibold text-orange-400 uppercase dark:text-white"
-            >
+            <h2 class="mb-6 text-md font-semibold text-orange-400 uppercase dark:text-white">
               Service
             </h2>
             <ul class="text-black dark:text-gray-400 font-medium">
@@ -42,55 +38,50 @@
             </ul>
           </div>
           <div>
-            <h2
-              class="mb-6 text-md font-semibold text-orange-400 uppercase dark:text-white"
-            >
+            <h2 class="mb-6 text-md font-semibold text-orange-400 uppercase dark:text-white">
               About Us
             </h2>
             <ul class="text-black dark:text-gray-400 font-medium">
               <li>
-                <a href="/terms" class="hover:text-orange-400"
-                  >Terms & Conditions</a
-                >
+                <a href="#About" class="hover:text-orange-400">About Us</a>
               </li>
             </ul>
           </div>
           <div>
-            <h2
-              class="mb-6 text-md font-semibold text-orange-400 uppercase dark:text-white"
-            >
-              Contact Us
+            <h2 class="mb-6 text-md font-semibold text-orange-400 uppercase dark:text-white">
+              Terms
             </h2>
-            <ul
-              class="text-black dark:text-gray-400 font-medium flex space-x-4"
-            >
-              <li class="">
-                <a
-                  href="https://www.facebook.com/profile.php?id=61556244512123&mibextid=LQQJ4d"
-                  ><Icon
-                    name="fa6-brands:square-facebook"
-                    class="w-8 h-8 hover:text-orange-400"
-                /></a>
-              </li>
-              <li class="">
-                <a
-                  href="https://www.instagram.com/smartbox_cambodia?igsh=MWtzZzhraHh1YjRqbQ=="
-                  ><Icon
-                    name="fa6-brands:square-instagram"
-                    class="w-8 h-8 hover:text-orange-400"
-                /></a>
+            <ul class="text-black dark:text-gray-400 font-medium">
+              <li>
+                <a href="/terms" class="hover:text-orange-400">Terms & Conditions</a>
               </li>
             </ul>
           </div>
         </div>
       </div>
       <hr class="my-6 border-black sm:mx-auto dark:border-white lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <span class="text-sm text-black sm:text-center dark:text-gray-400"
-          >© 2024
-          <a href="https://flowbite.com/" class="hover:underline">SmartBox™</a>.
-          All Rights Reserved.
-        </span>
+      <div class="sm:flex sm:items-center sm:justify-between grid-cols-6">
+        <div>
+          <ul class="text-black dark:text-gray-400 font-medium flex space-x-4">
+            <h2 class="ml-12 mb-6 text-lg font-extrabold">Contact Us</h2>
+            <li class="">
+              <a href="https://www.facebook.com/profile.php?id=61556244512123&mibextid=LQQJ4d">
+                <Icon name="fa6-brands:square-facebook" class="w-8 h-8 hover:text-orange-400" />
+              </a>
+            </li>
+            <li class="">
+              <a href="https://www.instagram.com/smartbox_cambodia?igsh=MWtzZzhraHh1YjRqbQ==">
+                <Icon name="fa6-brands:square-instagram" class="w-8 h-8 hover:text-orange-400" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <span class="text-sm text-black sm:text-center dark:text-gray-400">© 2024
+            <a href="https://flowbite.com/" class="hover:underline">SmartBox™</a>.
+            All Rights Reserved.
+          </span>
+        </div>
       </div>
     </div>
   </footer>
